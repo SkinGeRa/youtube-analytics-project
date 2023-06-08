ab045660d1fac1a966f9eecfdbdf105c595214fe
Nickolay <psyangel93@mail.ru>
2023-06-08 23:30:13 +0300

    update file

--- src/channel.py
+++ src/channel.py
@@ -8,9 +8,17 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.API_KEY = os.getenv('YT_API_KEY')
+        self.youtube = build('youtube', 'v3', developerKey=self.API_KEY)
         self.channel_id = channel_id
-        api_key: str = os.getenv('YT_API_KEY')
-        self.youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.subscriberCount = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.viewCount = self.channel["items"][0]["statistics"]["viewCount"]
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
