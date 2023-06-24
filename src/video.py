e7e64550a06d65c041e05046690a093595c00f47
Nickolay <psyangel93@mail.ru>
2023-06-24 19:34:04 +0300

    Add file video

---
+++ src/video.py
@@ -0,0 +1,30 @@
+import os
+from googleapiclient.discovery import build
+
+
+class Video:
+    """Класс для видео"""
+
+    def __init__(self, video_id):
+        """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
+        self.API_KEY = os.getenv('YT_API_KEY')
+        self.youtube = build('youtube', 'v3', developerKey=self.API_KEY)
+        self.video_id = video_id
+        self.video = self.youtube.videos().list(id=self.video_id,
+                                                part='snippet,statistics,contentDetails,topicDetails').execute()
+        self.title = self.video["items"][0]["snippet"]["title"]
+        self.url = f"https://www.youtube.com/channel/{self.video_id}"
+        self.viewers = self.video["items"][0]["statistics"]["viewCount"]
+        self.likes = int(self.video["items"][0]["statistics"]["likeCount"])
+
+    def __str__(self):
+        return self.title
+
+
+class PLVideo(Video):
+    """Класс для плейлиста"""
+
+    def __init__(self, video_id, playlist_id):
+        """Наследует атрибуты класса Video, а также инициализирует id плейлиста"""
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
