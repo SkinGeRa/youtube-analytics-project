8a75b009e7bb91fa259fb459f607b255cd9e1b1f
Nickolay <psyangel93@mail.ru>
2023-06-28 23:12:05 +0300

    Update file

--- src/playlist.py
+++ src/playlist.py
@@ -1,7 +1,6 @@
 import os
 from googleapiclient.discovery import build
-from src.channel import Channel
-import isodate
+# import isodate
 
 
 class PlayList:
@@ -10,8 +9,15 @@ class PlayList:
         self.API_KEY = os.getenv('YT_API_KEY')
         self.youtube = build('youtube', 'v3', developerKey=self.API_KEY)
         self.playlist_id = playlist_id
-        self.playlists_info = Channel.get_service().playlists().list(part='snippet', id=playlist_id).execute()
-        print(self.playlists_info)
+        self.playlists_info = self.youtube.playlists().list(part='snippet', id=playlist_id).execute()
+        self.playlist_videos = self.youtube.playlistItems().list(playlistId=playlist_id,
+                                                                 part='contentDetails, snippet',
+                                                                 maxResults=50,
+                                                                 ).execute()
+        self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
+        self.video_response = self.youtube.videos().list(part='contentDetails,statistics',
+                                                         id=','.join(self.video_ids)
+                                                         ).execute()
         self.title = self.playlists_info["items"][0]["snippet"]["title"]
         self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
 
@@ -19,4 +25,11 @@ class PlayList:
         pass
 
     def show_best_video(self):
-        pass
+        max_liked = 0
+        most_liked_video = 0
+        for video in self.video_response['items']:
+            if int(video['statistics']['likeCount']) > max_liked:
+                most_liked_video = video['id']
+                max_liked = int(video['statistics']['likeCount'])
+
+        return f"https://youtu.be/{most_liked_video}"
